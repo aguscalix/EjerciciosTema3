6f4bdcdc67969246bd81f1219d151399b331d58c
Agus <aguscalix@gmail.com>
2022-07-27 12:12:12 -0600

    Función y clase creada

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,29 @@ public class Main {
     public static void main(String[] args) {
 
 
+        // Llamando la función para sumar 3 números
+        int total_suma = suma(2, 3, 4);
+        System.out.println(total_suma);
+
+        // Crear instancia de un coche y añadir una puerta
+        Coche miCoche = new Coche();
+        miCoche.addPuerta();
+        System.out.println(miCoche.puertas);
+
     }
 
+    // Función para sumar 3 números
+    public static int suma(int a, int b, int c){
+        return a + b + c;
+    }
+
+}
+
+class Coche {
+    public  int puertas = 0;
+
+    public void addPuerta() {
+        this.puertas++;
+    }
 
 }
